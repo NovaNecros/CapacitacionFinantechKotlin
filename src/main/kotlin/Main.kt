83dae3b32297547609c1c87c1fcaf6f9bd2a9cb8
NovaNecros <uchihaespurr@gmail.com>
2025-03-19 15:06:44 -0600

    Le agregué mas funciones, cambie los datos de enteros a dobles y le puse comentarios.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,33 @@
 
-fun sumar(x:Int, y:Int) : Int
+fun sumar(x:Double, y:Double) : Double = x+y
+
+fun multi(x:Double, y:Double) : Double = x*y
+
+//raiz cuadrada de un número
+fun raiz(x:Double) : Double = Math.sqrt(x)
+
+//Calcula el promedio de dos números
+fun mediaAritmetica(x:Double, y:Double) : Double
 {
-    return x+y;
+    return multi(0.5, sumar(x,y))
 }
 
-fun multi(x:Int, y:Int) : Int = x*y
+//Multiplica dos números y saca la raíz cuadrada del resultado
+fun mediaGeometrica(x:Double, y:Double) : Double
+{
+    return raiz(multi(x,y))
+}
 
 
 fun main()
 {
-    val x : Int = 34;
-    val y : Int = 42;
+    val x : Double = 16.0;
+    val y : Double = 4.0;
 
     println("\nLa suma de ${x} y ${y} es ${sumar(x,y)}")
     println("El producto de ${x} y ${y} es ${multi(x,y)}")
+    println("La raíz de ${x} es: ${raiz(x)}")
+    println("La raíz de ${y} es: ${raiz(y)}")
+    println("La media aritmética de ${x} y ${y} es ${mediaAritmetica(x,y)}")
+    println("La media geométrica de ${x} y ${y} es ${mediaGeometrica(x,y)}")
 }
