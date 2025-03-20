88a7f7032a0d0001b88ee130a2b6620b59f12c41
NovaNecros <uchihaespurr@gmail.com>
2025-03-20 08:55:42 -0600

    falta el bubblesort

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,26 @@
-fun main()
+fun bubbleSort(numeros : Array<Int>, n : Int)
 {
-    println(":)")
+    //TODO
+}
+
+fun imprimirDatos(numeros : Array<Int>, n : Int)
+{
+    for(i in 0 until n)
+    {
+        print(numeros[i])
+    }
+    println()
+}
+
+fun main(args : Array<String>)
+{
+    var numeros :Array<Int> = arrayOf(9,6,7,2,5,3)
+    var tam = numeros.size
+
+    imprimirDatos(numeros, tam)
+    println("BubbleSort:")
+    bubbleSort(numeros, tam)
+    imprimirDatos(numeros, tam)
+
+    return
 }
