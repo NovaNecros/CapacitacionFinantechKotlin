094a13de5618c4e06cf3360a34d0dd45063e00fa
NovaNecros <uchihaespurr@gmail.com>
2025-03-20 09:00:13 -0600

    Ya sirve.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 fun bubbleSort(numeros : Array<Int>, n : Int)
 {
-    //TODO
+    for(i in 0 until n-1)
+    {
+        for(j in 0 until n-1-i)
+        {
+            if(numeros[j] > numeros[j+1])
+            {
+                val temp = numeros[j]
+                numeros[j] = numeros[j+1]
+                numeros[j+1] = temp
+            }
+        }
+    }
 }
 
 fun imprimirDatos(numeros : Array<Int>, n : Int)
