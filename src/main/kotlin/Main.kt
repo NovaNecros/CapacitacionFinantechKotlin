e1b745a9ca5706864ed6cf00b8744df2ec9fda56
NovaNecros <uchihaespurr@gmail.com>
2025-03-21 13:57:48 -0600

    Quedó el main de prueba.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ class Alumno(val nombre : String, val apellidoP : String, val apellidoM : String
     var materiaId : Int = 0
     var calificacion : Int = 0
 
-    fun getNombre() : String = this.nombre + " " + this.apellidoP + " " + this.apellidoM
+    fun getNombreAlumno() : String = this.nombre + " " + this.apellidoP + " " + this.apellidoM
 
     fun setMateria(materiaId : Int)
     {
@@ -21,15 +21,15 @@ class Alumno(val nombre : String, val apellidoP : String, val apellidoM : String
 
     fun getCalif() : Int = this.calificacion
 
-    override fun toString() : String = "Nombre: ${this.getNombre()}" +
-            "\nMateria: ${this.getMateria()}\nCalificación: ${this.getCalif()}"
+    override fun toString() : String = "\tNombre: ${this.getNombreAlumno()}" +
+            "\n\tMateria: ${this.getMateria()}\n\tCalificación: ${this.getCalif()}"
 }
 
 class Materia(val nombre : String, val id : Int)
 {
-    fun getNombre() : String = this.nombre
-    fun getId() : Int = this.id
-    override fun toString() : String = this.getId().toString() + " " + this.getNombre()
+    fun getNombreMateria() : String = this.nombre
+    fun getIdMateria() : Int = this.id
+    override fun toString() : String = this.getIdMateria().toString() + " " + this.getNombreMateria()
 }
 
 class Calificacion(val calif : String)
@@ -41,10 +41,10 @@ class Calificacion(val calif : String)
 
         for(materia in materias) {
             cont2 = 0
-            println(materia)
+            println("\n${materia}\n")
             for (alumno in alumnos) {
                 if (cont1 == alumnos[cont2].getMateria()) {
-                    println("\t${alumno}")
+                    println("${alumno}\n")
                 }
                 ++cont2
             }
@@ -56,5 +56,34 @@ class Calificacion(val calif : String)
 
 fun main()
 {
+    var alumnos = mutableListOf<Alumno>()
+    var materias = mutableListOf<Materia>()
+    var calificacion = Calificacion("1")
 
+    alumnos.add(Alumno("Morgan", "Jolly", "Martínez", 0))
+    alumnos.add(Alumno("abigail", "sánchez", "lópez", 1))
+    alumnos.add(Alumno("Nancy", "Prado", "Martínez", 2))
+    alumnos.add(Alumno("Michelle", "Pani", "Navarro", 3))
+    alumnos.add(Alumno("Mariana", "Morfín", "Márquez", 4))
+    alumnos.add(Alumno("Natalia", "Jolly", "Martínez", 5))
+
+    materias.add(Materia("Análisis y Diseño de Algoritmos", 1))
+    materias.add(Materia("Ecuaciones Diferenciales Parciales", 2))
+    materias.add(Materia("Dibujitos", 3))
+
+    alumnos[0].setMateria(1)
+    alumnos[1].setMateria(1)
+    alumnos[2].setMateria(2)
+    alumnos[3].setMateria(3)
+    alumnos[4].setMateria(2)
+    alumnos[5].setMateria(2)
+
+    alumnos[0].setCalif(10)
+    alumnos[1].setCalif(10)
+    alumnos[2].setCalif(5)
+    alumnos[3].setCalif(8)
+    alumnos[4].setCalif(9)
+    alumnos[5].setMateria(9)
+
+    calificacion.imprimir(materias, alumnos)
 }
