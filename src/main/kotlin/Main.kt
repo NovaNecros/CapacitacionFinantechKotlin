2b48e975348ac0246f04662f66456d515d72ad18
NovaNecros <uchihaespurr@gmail.com>
2025-03-21 13:31:17 -0600

    Ya están las clases materia y calificación.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,11 @@
 package org.example
 
-class Alumno(val nombre : String, val apellidoP : String,
-             val apellidoM : String, val id : Int)
+class Alumno(val nombre : String, val apellidoP : String, val apellidoM : String, val id : Int)
 {
     var materiaId : Int = 0
     var calificacion : Int = 0
 
-    fun getNombre() : String = this.nombre + " " + this.apellidoP +
-            " " + this.apellidoM
+    fun getNombre() : String = this.nombre + " " + this.apellidoP + " " + this.apellidoM
 
     fun setMateria(materiaId : Int)
     {
@@ -24,8 +22,36 @@ class Alumno(val nombre : String, val apellidoP : String,
     fun getCalif() : Int = this.calificacion
 
     override fun toString() : String = "Nombre: ${this.getNombre()}" +
-            "\nMateria: ${this.getMateria()}\n" +
-            "Calificación: ${this.getCalif()}"
+            "\nMateria: ${this.getMateria()}\nCalificación: ${this.getCalif()}"
+}
+
+class Materia(val nombre : String, val id : Int)
+{
+    fun getNombre() : String = this.nombre
+    fun getId() : Int = this.id
+    override fun toString() : String = this.getId().toString() + " " + this.getNombre()
+}
+
+class Calificacion(val calif : String)
+{
+    fun imprimir(materias : MutableList<Materia>, alumnos : MutableList<Alumno>)
+    {
+        var cont1 : Int = 1
+        var cont2 : Int
+
+        for(materia in materias) {
+            cont2 = 0
+            println(materia)
+            for (alumno in alumnos) {
+                if (cont1 == alumnos[cont2].getMateria()) {
+                    println("\t${alumno}")
+                }
+                ++cont2
+            }
+            ++cont1
+        }
+    }
+    override fun toString() : String = "${calif}"
 }
 
 fun main()
