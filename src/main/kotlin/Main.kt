e75ec14c905da393427dd9f9abeb1edbeb96fbd7
NovaNecros <uchihaespurr@gmail.com>
2025-03-21 12:43:09 -0600

    Versión con programación dinámica y con una fórmula cerrada.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,55 @@
+//phi = 1.618, proporción aúrea
+val phi : Double = 0.5*(1.0+Math.sqrt(5.0))
+val invrt5 : Double = 1.0/Math.sqrt(5.0)
+
+//Recursion: tiempo - O(phi^n), memoria O(1)
 fun fibonacci(n : Int) : Int
 {
-    return if(n == 0 || n == 1) {1}
+    return if(n <= 1) {n}
     else fibonacci(n-1)+fibonacci(n-2)
 }
 
-fun main()
+//Programación dinámica: tiempo - O(n), memoria O(n)
+fun fibonacciDinamico(n : Int, fiboMem : MutableList<Int>) : Int
 {
-    var temp : Int
-    var x : Int = 1
-    val limite : Int
-
-    println("Ingrese número límite de recursión")
-    val opcion : String = readlnOrNull() ?: ""
-    limite = opcion.toInt()
-
-    while(x <= limite)
+    if(fiboMem.isEmpty())
     {
-        temp = fibonacci(x)
-        println("F${x} = ${temp}")
-        ++x
+        fiboMem.add(0)
+        fiboMem.add(1)
     }
 
-    return
+    if(fiboMem.size <= n)
+    {
+        fiboMem.add(fibonacciDinamico(n-1, fiboMem)
+                + fibonacciDinamico(n-2, fiboMem))
+    }
+
+    return fiboMem[n]
+}
+
+//Fórmula de Binet: Tiempo - O(1), Memoria - O(1)
+fun fibonacciBinet(n : Int) : Int =
+    Math.round(Math.pow(phi, n.toDouble())*invrt5).toInt()
+
+fun main()
+{
+    print("\nn:\t")
+    val n : Int = (readlnOrNull() ?: "").toInt()
+
+    println("\nRecursión O(phi^n):")
+    for(i in 0 until n)
+    {
+        print("${fibonacci(i)}, ")
+    }
+    print("${fibonacci(n)}\n")
+
+
+    println("\nProgramación Dinámica O(n):")
+    var fiboMem = mutableListOf<Int>()
+    fibonacciDinamico(n, fiboMem)
+    println(fiboMem.toString())
+
+
+    println("\nFórmula de Binet O(1):")
+    println("F${n} = ${fibonacciBinet(n)}")
 }
