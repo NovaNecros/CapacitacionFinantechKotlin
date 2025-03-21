43813dfb3f4658f6ba6eb5c620d452f8802095b9
NovaNecros <uchihaespurr@gmail.com>
2025-03-21 08:51:36 -0600

    Había escrito mal Fahrenheit.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,26 +5,23 @@ fun areaTriangulo(base : Double, altura : Double) : Double = 0.5*base*altura
 fun areaTriangulo(lado1 : Double, lado2 : Double, lado3 : Double) : Double
 {
     val S : Double = 0.5*(lado1 + lado2 + lado3)
-    val A2 : Double = S*(S-lado1)*(S-lado2)*(S-lado3)
-
-    return Math.sqrt(A2)
-
+    return Math.sqrt(S*(S-lado1)*(S-lado2)*(S-lado3))
 }
 
-fun celsiusToFarenheit(c : Double) : Double = c*1.8 + 32
+fun celsiusToFahrenheit(c : Double) : Double = c*1.8 + 32
 fun celsiusToKelvin(c : Double) : Double = c + 273.16
 fun celsiusToRankine(c : Double) : Double = 1.8*c + 491.67
 
-fun farenheitToCelsius(f : Double) : Double = (f-32)/1.8
-fun farenheitToKelvin(f : Double) : Double = (f+459.67)/1.8
-fun farenheitToRankine(f : Double) : Double = f + 459.67
+fun fahrenheitToCelsius(f : Double) : Double = (f-32)/1.8
+fun fahrenheitToKelvin(f : Double) : Double = (f+459.67)/1.8
+fun fahrenheitToRankine(f : Double) : Double = f + 459.67
 
 fun kelvinToCelsius(k : Double) : Double = k - 273.16
-fun kelvinToFarenheit(k : Double) : Double = (k-273.16)*1.8 + 32
+fun kelvinToFahrenheit(k : Double) : Double = (k-273.16)*1.8 + 32
 fun kelvinToRankine(k : Double) : Double = 1.8*k
 
 fun rankineToCelsius(r : Double) : Double = (r-491.67)/1.8
-fun rankineToFarenheit(r : Double) : Double = r - 459.67
+fun rankineToFahrenheit(r : Double) : Double = r - 459.67
 fun rankineToKelvin(r : Double) : Double = r/1.8
 
 
@@ -40,31 +37,31 @@ fun main()
     val altura : Double = 4.0
     val hipotenusa : Double = 5.0
     println("Lados: ${base}, ${altura}, ${hipotenusa} (es triángulo rectángulo)")
-    println("Área (b*h/2): ${areaTriangulo(4.0,5.0)}")
+    println("Área (b*h/2): ${areaTriangulo(base, altura)}")
     println("Área (fórmula de Heron): ${areaTriangulo(base, altura, hipotenusa)}\n\n")
 
 
     val c : Double = 21.0
     println("Temperatura: ${c} °C")
-    println("${celsiusToFarenheit(c)} °F")
+    println("${celsiusToFahrenheit(c)} °F")
     println("${celsiusToKelvin(c)} K")
     println("${celsiusToRankine(c)} °R\n")
 
     val f : Double = 21.0
     println("Temperatura: ${f} °F")
-    println("${farenheitToCelsius(f)} °C")
-    println("${farenheitToKelvin(f)} K")
-    println("${farenheitToRankine(f)} °R\n")
+    println("${fahrenheitToCelsius(f)} °C")
+    println("${fahrenheitToKelvin(f)} K")
+    println("${fahrenheitToRankine(f)} °R\n")
 
     val k : Double = 21.0
     println("Temperatura: ${k} K")
     println("${kelvinToCelsius(k)} °C")
-    println("${kelvinToFarenheit(k)} °F")
+    println("${kelvinToFahrenheit(k)} °F")
     println("${kelvinToRankine(f)} °R\n")
 
     val r : Double = 21.0
     println("Temperatura: ${r} °R")
     println("${rankineToCelsius(r)} °C")
-    println("${rankineToFarenheit(r)} °F")
+    println("${rankineToFahrenheit(r)} °F")
     println("${rankineToKelvin(r)} K")
 }
